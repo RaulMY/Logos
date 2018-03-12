c3a04756a559f80c83bc987fa822e3538ed87e00
Raul Merino <merinoyrr@gmail.com>
2018-03-12 11:05:21 -0600

    test

--- server/routes/arcky.js
+++ server/routes/arcky.js
@@ -55,7 +55,7 @@ router.post('/webhook', (req, res) => {
     
       // Get the sender PSID
       let sender_psid = webhook_event.sender.id;
-      console.log(webhook_event.sender)
+      console.log(webhook_event)
       console.log('Sender PSID: ' + sender_psid);
 
       // Check if the event is a message or postback and
