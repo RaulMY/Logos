134504267ab6bdb702536943bfa0c2b555e2a3b2
Raul Merino <merinoyrr@gmail.com>
2018-03-12 10:29:26 -0600

    Hai

--- server/routes/arcky.js
+++ server/routes/arcky.js
@@ -1,9 +1,12 @@
+require("dotenv").config();
 var express = require('express');
 var router = express.Router();
-
+const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+
+  console.log(PAGE_ACCESS_TOKEN);
 });
 
 // Adds support for GET requests to our webhook
@@ -45,10 +48,14 @@ router.post('/webhook', (req, res) => {
     // Iterates over each entry - there may be multiple if batched
     body.entry.forEach(function(entry) {
 
-      // Gets the message. entry.messaging is an array, but 
-      // will only ever contain one message, so we get index 0
+      // Gets the body of the webhook event
       let webhook_event = entry.messaging[0];
       console.log(webhook_event);
+    
+      // Get the sender PSID
+      let sender_psid = webhook_event.sender.id;
+      console.log('Sender PSID: ' + sender_psid);
+    
     });
 
     // Returns a '200 OK' response to all requests
@@ -59,5 +66,20 @@ router.post('/webhook', (req, res) => {
   }
 
 });
+
+// Handles messages events
+function handleMessage(sender_psid, received_message) {
+
+}
+
+// Handles messaging_postbacks events
+function handlePostback(sender_psid, received_postback) {
+
+}
+
+// Sends response messages via the Send API
+function callSendAPI(sender_psid, response) {
+  
+}
 
 module.exports = router;
